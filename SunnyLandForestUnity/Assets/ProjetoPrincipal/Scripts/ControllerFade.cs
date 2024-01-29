23ed6cedace939172e4092f6dd441717df391191
Jonathan Gonçalves <jonathan.g.oliveira13@gmail.com>
2024-01-29 15:29:17 -0300

    reate FadeIn in menu

---
+++ SunnyLandForestUnity/Assets/ProjetoPrincipal/Scripts/ControllerFade.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ControllerFade : MonoBehaviour
+{
+
+    public static ControllerFade instanceFade;
+    public Image imagemFade;
+    public Color corInicial;
+    public Color corFinal;
+    public float duracaoFade;
+    public bool isFade;
+    private float _tempo;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        //instanceFade = GetComponent<ControllerFade>();
+        instanceFade = this;
+    }
+
+    IEnumerator InicioFade()
+    {
+        isFade = true;
+        _tempo = 0f;
+        while (_tempo <= duracaoFade) 
+        { 
+            imagemFade.color = Color.Lerp(corInicial, corFinal, _tempo / duracaoFade);
+            _tempo = _tempo + Time.deltaTime;
+            yield return null;
+        }
+        isFade = false;
+
+    }
+    void Start()
+    {
+        StartCoroutine(InicioFade());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
